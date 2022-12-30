d2f7f58c49e79caf85ebef03f80f1e7cd8487bba
Maximiliano Greco <mmngreco@gmail.com>
2022-12-30 20:52:12 +0100

    auto commit

--- vim/init.lua
+++ vim/init.lua
@@ -922,11 +922,11 @@ augroup END
 -- [[ terminal ]]
 -- terminal settings
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
-vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', {noremap = true})
-vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', {noremap = true})
-vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', {noremap = true})
+-- vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', {noremap = true})
+-- vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', {noremap = true})
+-- vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', {noremap = true})
 -- vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', {noremap = true})
-vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>w', {noremap = true})
+-- vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>w', {noremap = true})
 
 
 -- [[ LSP ]]
