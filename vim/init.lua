5e727003d2da9b900608fc1587b73ee2f6cd49e8
Maximiliano Greco <mmngreco@gmail.com>
2023-01-07 18:05:14 +0100

    auto update

--- vim/init.lua
+++ vim/init.lua
@@ -184,7 +184,7 @@ require('packer').startup(function(use)
 
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
   }
 
   use { -- Highlight, edit, and navigate code
