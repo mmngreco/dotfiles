3d6cc0a790904887e122efe49637fa4fbd047c87
Maximiliano Greco <mmngreco@gmail.com>
2023-01-18 11:05:04 +0100

    auto update

--- vim/init.lua
+++ vim/init.lua
@@ -13,6 +13,19 @@ end
 
 require('packer').startup(function(use)
   -- Package manager
+  use {
+    'chipsenkbeil/distant.nvim',
+    config = function()
+      require('distant').setup {
+        -- Applies Chip's personal settings to every machine you connect to
+        --
+        -- 1. Ensures that distant servers terminate with no connections
+        -- 2. Provides navigation bindings for remote directories
+        -- 3. Provides keybinding to jump into a remote file's parent directory
+        ['*'] = require('distant.settings').chip_default()
+      }
+    end
+  }
 
   use {'dhruvasagar/vim-table-mode'}
 
