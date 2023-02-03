6824c98af8d05e4ab908f1f2cb90bc4c13821716
Maximiliano Greco <mmngreco@gmail.com>
2023-02-03 12:03:45 +0100

    auto update

--- vim/init.lua
+++ vim/init.lua
@@ -1611,4 +1611,53 @@ vim.keymap.set('n', '<leader>y', '"+yy', {noremap = true, desc = 'copy to system
 vim.keymap.set('v', '<leader>y', '"+y', {noremap = true, desc = 'copy to system clipboard'})
 vim.keymap.set('n', '<leader>yf', ':let @+ = expand("%:p")<cr>', {noremap = true, desc = 'yank filename/buffer path'})
 
+
+-- [[ large files ]]
+local LargeFile = vim.api.nvim_create_augroup("LargeFileAutocmds", {})
+local old_eventignore = vim.o.eventignore;
+local largefile_opened = false;
+
+
+vim.api.nvim_create_autocmd({"BufReadPre"}, {
+	group = LargeFile,
+	callback = function(ev)
+		if ev.file then
+			local status, size = pcall(function() return vim.loop.fs_stat(ev.file).size end)
+			if status and size > 1024 * 1024 then -- large file
+				vim.wo.wrap = false
+				old_eventignore = vim.o.eventignore
+				largefile_opened = true
+				vim.o.eventignore = 'FileType'
+				vim.bo.swapfile = false
+				vim.bo.bufhidden = 'unload'
+				vim.bo.buftype = 'nowrite'
+				vim.bo.undolevels = -1
+			end
+		end
+	end
+})
+
+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
+	group = LargeFile,
+	callback = function(ev)
+		if largefile_opened then
+			largefile_opened = false
+			vim.o.eventignore = nil
+		end
+	end
+})
+
+
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+	group = LargeFile,
+	callback = function(ev)
+		local byte_size = vim.api.nvim_buf_get_offset(ev.buf, vim.api.nvim_buf_line_count(ev.buf))
+		if byte_size > 1024 * 1024 then
+			vim.cmd('NoMatchParen')
+		else
+			vim.cmd('DoMatchParen')
+		end
+	end
+})
+
 -- vim:ts=2 sts=2 sw=2 et
