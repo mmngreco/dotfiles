d8735df2db71a161ce368638dc5f22c94e6d82db
Maximiliano Greco <mmngreco@gmail.com>
2023-03-30 15:55:33 +0200

    auto update

--- vim/init.lua
+++ vim/init.lua
@@ -435,9 +435,44 @@ function SetColorScheme(colorscheme)
     vim.cmd('colorscheme modus-vivendi')
 
   elseif colorscheme == 'catppuccin' then
-    vim.o.termguicolors = true
-    vim.g.catppuccin_flavour = 'mocha'
-    vim.cmd [[colorscheme catppuccin]]
+    require("catppuccin").setup {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      term_colors = true,
+      transparent_background = false,
+      no_italic = false,
+      no_bold = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      highlight_overrides = {
+        mocha = function(C)
+          return {
+            TabLineSel = { bg = C.pink },
+            CmpBorder = { fg = C.surface2 },
+            Pmenu = { bg = C.none },
+            TelescopeBorder = { link = "FloatBorder" },
+          }
+        end,
+      },
+    }
+    vim.cmd.colorscheme "catppuccin"
 
   elseif colorscheme == 'tokyonight' then
     vim.cmd [[colorscheme tokyonight]]
