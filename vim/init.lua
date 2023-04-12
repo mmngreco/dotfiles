3f3b06f6119e4db42ea133f1449dd36c63e89140
Maximiliano Greco <mmngreco@gmail.com>
2023-04-12 15:43:58 +0200

    auto commit

--- vim/init.lua
+++ vim/init.lua
@@ -23,13 +23,18 @@ require('packer').startup(function(use)
   --   end
   -- }
   -- Packer
+
   use({'mracos/mermaid.vim'})
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  use({ "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  })
 
 
   use({
     "jackMort/ChatGPT.nvim",
-    dependencies = {
+    requires = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
@@ -53,70 +58,6 @@ require('packer').startup(function(use)
       })
     end,
   })
-
-  -- use({
-  --   "jackMort/ChatGPT.nvim",
-  --   config = function() require("chatgpt").setup({
-  --    keymaps = {
-  --       close = { "<C-c>" },
-  --       submit = '<C-m>',
-  --       yank_last = "<C-y>",
-  --       yank_last_code = "<C-k>",
-  --       scroll_up = "<C-u>",
-  --       scroll_down = "<C-d>",
-  --       toggle_settings = "<C-o>",
-  --       new_session = "<C-n>",
-  --       cycle_windows = "<Tab>",
-  --       -- in the Sessions pane
-  --       select_session = "<Space>",
-  --       rename_session = "r",
-  --       delete_session = "d",
-  --     }
-  --   }) end,
-  --   requires = {
-  --     "MunifTanjim/nui.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-telescope/telescope.nvim"
-  --   }
-  -- })
-
-  -- use({
-  -- "jackMort/ChatGPT.nvim",
-  -- config = function()
-  --     require("chatgpt").setup({
-  --       yank_register = "+",
-  --       chat_layout = {
-  --         relative = "editor",
-  --         position = "0%",
-  --         size = {
-  --           height = "100%",
-  --           width = "100%",
-  --         },
-  --       },
-  --       keymaps = {
-  --           close = { "<C-c>"},
-  --           yank_last = "<C-y>",
-  --           yank_last_code = "<C-k>",
-  --           scroll_up = "<C-u>",
-  --           scroll_down = "<C-d>",
-  --           toggle_settings = "<C-o>",
-  --           new_session = "<C-n>",
-  --           cycle_windows = "<Tab>",
-  --           -- in the Sessions pane
-  --           select_session = "<Space>",
-  --           rename_session = "r",
-  --           delete_session = "d",
-  --           submit = '<C-m>',
-  --         }
-  --     })
-  --   end,
-  --   requires = {
-  --     "MunifTanjim/nui.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-telescope/telescope.nvim"
-  --   }
-  -- })
-
 
   use {'xiyaowong/telescope-emoji.nvim'}
   use { 'michaelb/sniprun', run = 'bash ./install.sh'}
