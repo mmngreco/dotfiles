0932f5d95aa83b955133060bccb573b3159becbd
Maximiliano Greco <mmngreco@gmail.com>
2023-05-29 21:37:16 +0200

    enh(macos): update setup scripts

--- macos/install/common.sh
+++ macos/install/common.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env sh
 brew install tmux
 brew install neovim
 brew install mamba
 brew install conda
 brew install docker
+brew install nodejs
+brew install google-cloud-sdk
--- macos/install/neovim.sh
+++ macos/install/neovim.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 brew install --HEAD neovim
 
