9368cbade7de90be5efdbc65b9491a3da87019bd
Maximiliano Greco <mmngreco@gmail.com>
2023-06-13 14:42:48 +0200

    auto update

---
+++ macos/install/pipx.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+brew install pipx --reinstall
+
+pipx install black
+pipx install cookiecutter
+pipx install copier
+pipx install flake8
+pipx install httpie
+pipx install isort
+pipx install jupytext
+pipx install lookatme
+# pipx install pdftotext
+pipx install pydocstyle
+pipx install pydoro
+pipx install ruff
+# pipx install s3cmd
+pipx install sgpt
+pipx install sqlparse
+pipx install trash-cli
+pipx install vdsql
+pipx install visidata
