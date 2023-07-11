6f5d622dbf0ae0fc70aa0704dbc358c4ed308202
Maximiliano Greco <mmngreco@gmail.com>
2023-07-11 11:58:53 +0200

    auto update

--- vim/init.lua
+++ vim/init.lua
@@ -116,6 +116,21 @@ require('packer').startup(function(use)
     },
     config = function()
       require("chatgpt").setup({
+        openai_params = {
+          model = "gpt-4",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 300,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        -- openai_edit_params = {
+        --   model = "code-davinci-edit-001",
+        --   temperature = 0,
+        --   top_p = 1,
+        --   n = 1,
+        -- },
         chat = {
           keymaps = {
             close = { "<C-c>", },
