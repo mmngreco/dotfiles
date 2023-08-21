aa526ae0b0fd341e3a3431776f4e903964958039
Maximiliano Greco <mmngreco@gmail.com>
2023-08-21 17:08:31 +0200

    auto update

--- ubuntu/rofi/bashgpt.sh
+++ ubuntu/rofi/bashgpt.sh
@@ -2,31 +2,17 @@
 # GPT-3 ( text-curie-001 text-babbage-001 text-ada-001 davinci curie babbage ada)
 # GPT-3.5 ( gpt-3.5-turbo gpt-3.5-turbo-0301 text-davinci-003 text-davinci-002 code-davinci-002)
 # GPT-4 (gpt-4 gpt-4-0314 gpt-4-32k gpt-4-32k-0314 )
-#!/bin/bash
 
 OPENAI_API_KEY="${OPENAI_API_KEY:-PUT_YOUT_OPENAI_KEY_HERE}"
 model="gpt-3.5-turbo"
 temperature="0.7"
 
-theme_options=(
-    -theme-str '#window {background: #282C34;}'             # fondo de la ventana
-    -theme-str '#prompt {text-color: #61AFEF;}'             # color del texto del prompt
-    -theme-str '#entry {text-color: #ABB2BF;}'              # color del texto ingresado
-    -theme-str '#normal {background-color: #282C34; text-color: #ABB2BF;}'     # color de la opción normal
-    -theme-str '#active {background-color: #353B45; text-color: #ABB2BF;}'     # color de la opción activa
-    -theme-str '#urgent {background-color: #E06C75; text-color: #ABB2BF;}'     # color de la opción urgente
-    -theme-str '#selected {background-color: #61AFEF; text-color: #282C34;}'   # color de la opción seleccionada
-    -theme-str '#message {text-color: #98C379;}'            # color del mensaje
-)
-
-
-rofi_prompt() {
-    selected=$( rofi -e -p "$model" "${theme_options[@]}")
-    echo $selected
+prompt() {
+    selected=$(rofi -theme $DOTFILES/rofi/onedark.rasi -dmenu -p "$model")
+    echo "$selected"
 }
 
-
-query="${1:-$(rofi_prompt)}"
+query="${1:-$(prompt)}"
 query=$( echo "$query" | tr '\"' '`' )
 
 [[ -z "$query" ]] && exit 1
