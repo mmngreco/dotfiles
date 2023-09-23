e2071ed9f7aeca5d69002c1c3eb99ca28cfeb291
Maximiliano Greco <mmngreco@gmail.com>
2023-09-23 17:48:52 +0200

    auto commit

---
+++ macos/home/.wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.window.maximize(0)
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 1
+
+
+-- and finally, return the configuration to wezterm
+return config
