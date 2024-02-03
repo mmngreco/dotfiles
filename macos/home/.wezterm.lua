e418946c2a313eddf592a6e3799b85be03c848c4
Maximiliano Greco <mmngreco@gmail.com>
2024-02-03 11:12:24 +0100

    add alacritty config

--- macos/home/.wezterm.lua
+++ macos/home/.wezterm.lua
@@ -10,12 +10,11 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 20
 
 -- and finally, return the configuration to wezterm
 return config
