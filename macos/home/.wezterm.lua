9569c9e2864d39396bdd06d51f3ed591534a021c
Maximiliano Greco <mmngreco@gmail.com>
2024-03-12 17:45:39 +0100

    update

--- macos/home/.wezterm.lua
+++ macos/home/.wezterm.lua
@@ -13,15 +13,14 @@ end
 -- For example, changing the color scheme:
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.6
 
 config.font = wezterm.font(
-    'CaskaydiaCove Nerd Font',
-    { stretch = 'Expanded', weight = 'Regular' }
+    'CaskaydiaCove Nerd Font', { stretch = 'Expanded', weight = 'Bold' }
   )
 
 -- config.color_scheme = 'iTerm2 Default'
-config.color_scheme = 'Ibm 3270 (High Contrast) (Gogh)'
+-- config.color_scheme = 'Ibm 3270 (High Contrast) (Gogh)'
 -- config.color_scheme = 'Wez'
 -- config.color_scheme = 'Vibrant Ink (Gogh)'
 -- config.color_scheme = 'tokyonight'
